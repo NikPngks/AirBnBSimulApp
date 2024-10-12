38cfec316d847a3597bb2aa365ef52de8c8fc7c3
polocoDP <panagakis10@gmail.com>
2024-10-12 23:22:01 +0300

    add a comment

--- src/AirBnBSimulApp_VScodePart/Master.java
+++ src/AirBnBSimulApp_VScodePart/Master.java
@@ -1,7 +1,7 @@
 package AirBnBSimulApp_VScodePart;
 
 import java.io.IOException;
-import java.net.ServerSocket;
+import java.net.ServerSocket; //
 import java.net.Socket;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
